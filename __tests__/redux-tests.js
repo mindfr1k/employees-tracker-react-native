b9d9932f954c148e7ffab785c02f470b3c718a46
Alex <zaychenko96y@outlook.com>
2020-03-28 23:35:17 +0200

    Added basic test for redux

---
+++ __tests__/redux-tests.js
@@ -0,0 +1,23 @@
+import 'react-native'
+
+import { REQUEST_START, REQUEST_FAIL, REQUEST_SUCCESS } from '../store/actions/action-types'
+import requestReducer from '../store/reducers/request'
+// Note: test renderer must be required after react-native.
+
+describe('redux reducer handles requests correctly', () => {
+  const store = { error: null, loading: false }
+  const reqPayload = { token: '12v345xt', role: 'sample' }
+  it('updates error and loading state on request', () => {
+    const newState = requestReducer(store, { type: REQUEST_START })
+    expect(newState.loading).toBe(true)
+  })
+  it('updates error and loading state on request fail', () => {
+    const newState = requestReducer(store, { type: REQUEST_FAIL, error: new Error('Error') })
+    expect(newState.loading).toBe(false)
+    expect(newState.error).toBeTruthy()
+  })
+  it('updates payload and loading state on request success', () => {
+    const newState = requestReducer(store, { type: REQUEST_SUCCESS, ...reqPayload })
+    expect(newState).toEqual({ error: null, loading: false, ...reqPayload })
+  })
+})
