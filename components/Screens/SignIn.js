8f7507d0ede7d5979bfb0a6a290a47b568fbe13f
Alex <zaychenko96y@outlook.com>
2020-04-01 04:36:26 +0300

    Refactored redux logic to hooks

--- components/Screens/SignIn.js
+++ components/Screens/SignIn.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ActivityIndicator, Alert } from 'react-native'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { authSignIn, clearRequestError } from '../../store/actions'
 import Form from '../UI/Form'
@@ -8,35 +8,32 @@ import TextInput from '../UI/TextInput'
 import withKeyboardDismiss from '../hoc/withKeyboardDismiss'
 import { CenteredContainer } from '../Styled'
 
-const SignIn = ({ loading, error, authSignIn, clearRequestError }) => {
-  console.log(error)
-  const form = (
-    <Form
-      action="Log In"
-      onSubmit={formData => authSignIn(formData)}>
-      <TextInput id="username" placeholder="Username" validation={{ required: true }}
-        autoCapitalize="none" />
-      <TextInput id="password" placeholder="Password" validation={{ required: true }} />
-    </Form>
-  )
+const SignIn = () => {
+  const [isRequestIdle, setIsRequestIdle] = useState(true)
+  const { loading, error } = useSelector(({ requestReducer: { loading, error } }) => ({ loading, error }))
+  const dispatch = useDispatch()
   return (
     <CenteredContainer>
-      {error && Alert.alert(error, 'Please, try again.', [{
-        text: 'OK',
-        onPress: () => clearRequestError()
-      }])}
       {loading
         ? <ActivityIndicator />
-        : !error && form}
-
+        : isRequestIdle && (
+          <Form
+            action="Log In"
+            onSubmit={formData => {
+              dispatch(authSignIn(formData))
+              setIsRequestIdle(false)
+            }}>
+            <TextInput id="username" placeholder="Username" validation={{ required: true }}
+              autoCapitalize="none" />
+            <TextInput id="password" placeholder="Password" validation={{ required: true }} />
+          </Form>
+        )}
+      {error && !isRequestIdle && Alert.alert(error, 'Please, try again.', [{
+        text: 'OK',
+        onPress: () => setIsRequestIdle(true)
+      }])}
     </CenteredContainer>
   )
 }
 
-const mapStateToProps = ({ requestReducer: { loading, error } }) => ({ loading, error })
-const mapDispatchToProps = dispatch => ({
-  authSignIn: formData => dispatch(authSignIn(formData)),
-  clearRequestError: () => dispatch(clearRequestError()),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(withKeyboardDismiss(SignIn))
+export default withKeyboardDismiss(SignIn)
